1a8eba59d8f6d66f2b5566c2b7fbc1e83e290476
catusan <carolinetuliosanches@gmail.com>
2023-11-01 17:31:02 -0300

    insere a nova versao do projeto integrado com mongodb

--- mulheres.js
+++ mulheres.js
@@ -1,60 +1,83 @@
 const express = require("express"); // aqui estou iniciando o express
-const router = express.Router(); //qui estou configurando a primeira parte da rota
-const { v4: uuidv4 } = require("uuid"); //iniciando criador de id
+const router = express.Router(); //aqui estou configurando a primeira parte da rota
+const conectaBncoDeDados = require("./bancoDeDados"); //Ligando ao arquivo banco de dados
+const cors = requisre("cors"); //aqui estou trazendo o pacote cors que permite consumir essa api no front end
+
+conectaBncoDeDados(); //chamando a função que conecta o banco de dados
+
+const Mulher = require("./mulherModel");
 
 const app = express(); //aqui estou iniciando o app
 app.use(express.json());
 
+app.use(cors());
+
 const porta = 3333; //qui estou criando a porta
 
-//aqui estou criando a lista inicial de mulheres
-const mulheres = [
-  {
-    id: "1",
-    nome: "Simara Conceição",
-    imagem: "https://bit.ly/3LJIyOF",
-    minibio: "Desenvolvedora e instrutora",
-  },
-
-  {
-    id: "2",
-    nome: "Iana Chan",
-    imagem: "https://bit.ly/3JCXBqP",
-    minibio: "CEO & Founder da PrograMaria",
-  },
-
-  {
-    id: "3",
-    nome: "Luana Pimentel",
-    imagem: "https://bit.ly/3FKpFaz",
-    minibio: "Senior Staff Software Engineer",
-  },
-
-  {
-    id: "4",
-    nome: "Caroline Tulio",
-    imagem: "https://i.imgur.com/PZTOwiQ.png",
-    minibio: "CRM Analyst e Desenvolvedora Web",
-  },
-];
-
 //GET
-function mostraMulheres(request, response) {
-  response.json(mulheres);
+async function mostraMulheres(request, response) {
+  try {
+    const mulheresVindasDoBancoDeDados = await Mulher.find();
+
+    response.json(mulheresVindasDoBancoDeDados);
+  } catch (erro) {
+    console.log(erro);
+  }
 }
 
 //POST
-function criaMulher(request, response) {
-  const novaMulher = {
-    id: uuidv4(),
+async function criaMulher(request, response) {
+  const novaMulher = new Mulher({
     nome: request.body.nome,
     imagem: request.body.imagem,
     minibio: request.body.minibio,
-  };
+    citacao: request.body.citacao,
+  });
 
-  mulheres.push(novaMulher);
+  try {
+    const mulherCriada = await novaMulher.save();
+    response.status(201).json(mulherCriada);
+  } catch (erro) {
+    console.log(erro);
+  }
+}
 
-  return response.json(mulheres);
+//PATCH
+async function corrigeMulher(request, response) {
+  try {
+    const mulherEncontrada = await Mulher.findById(request.params.id);
+
+    if (request.body.name) {
+      mulherEncontrada.nome = request.body.nome;
+    }
+
+    if (request.body.imagem) {
+      mulherEncontrada.imagem = request.body.imagem;
+    }
+
+    if (request.body.minibio) {
+      mulherEncontrada.minibio = request.body.minibio;
+    }
+
+    if (request.body.citacao) {
+      mulherEncontrada.citacao = request.body.citacao;
+    }
+
+    const mulherAtualizadaNoBancoDeDados = await mulherEncontrada.save();
+    response.json(mulherAtualizadaNoBancoDeDados);
+  } catch (erro) {
+    console.log(erro);
+  }
+}
+
+//DELETE
+async function deletaMulher(request, response) {
+  try {
+    await Mulher.findByIdAndDelete(request.params.id);
+    response.json({ mensagem: "Mulher deletada com sucesso!" });
+  } catch (erro) {
+    console.log(erro);
+  }
 }
 
 //PORTA
@@ -64,4 +87,6 @@ function mostraPorta() {
 
 app.use(router.get("/mulheres", mostraMulheres)); //configurei a rota GET /mulheres
 app.use(router.post("/mulheres", criaMulher)); //configurei a rota POST /mulheres
+app.use(router.patch("/mulheres/:id", corrigeMulher)); //configurei a rota PATCH /mulheres
+app.use(router.delete("/mulheres/:id", deletaMulher)); //configurei a rota DELETE /mulheres
 app.listen(porta, mostraPorta); //servidor ouvindo a porta
---
+++ primeiros_passos/mulheres.js
@@ -0,0 +1,112 @@
+const express = require("express"); // aqui estou iniciando o express
+const router = express.Router(); //qui estou configurando a primeira parte da rota
+const { v4: uuidv4 } = require("uuid"); //iniciando criador de id
+const conectaBncoDeDados = require("../bancoDeDados"); //Ligando ao arquivo banco de dados
+conectaBncoDeDados(); //chamando a função que conecta o banco de dados
+
+const app = express(); //aqui estou iniciando o app
+app.use(express.json());
+
+const porta = 3333; //qui estou criando a porta
+
+//aqui estou criando a lista inicial de mulheres
+const mulheres = [
+  {
+    id: "1",
+    nome: "Simara Conceição",
+    imagem: "https://bit.ly/3LJIyOF",
+    minibio: "Desenvolvedora e instrutora",
+  },
+
+  {
+    id: "2",
+    nome: "Iana Chan",
+    imagem: "https://bit.ly/3JCXBqP",
+    minibio: "CEO & Founder da PrograMaria",
+  },
+
+  {
+    id: "3",
+    nome: "Luana Pimentel",
+    imagem: "https://bit.ly/3FKpFaz",
+    minibio: "Senior Staff Software Engineer",
+  },
+
+  {
+    id: "4",
+    nome: "Caroline Tulio",
+    imagem: "https://i.imgur.com/PZTOwiQ.png",
+    minibio: "CRM Analyst e Desenvolvedora Web",
+  },
+];
+
+//GET
+function mostraMulheres(request, response) {
+  response.json(mulheres);
+}
+
+//POST
+function criaMulher(request, response) {
+  const novaMulher = {
+    id: uuidv4(),
+    nome: request.body.nome,
+    imagem: request.body.imagem,
+    minibio: request.body.minibio,
+  };
+  mulheres.push(novaMulher);
+
+  return response.json(mulheres);
+}
+
+//PATCH
+function corrigeMulher(request, response) {
+  function encontraMulher(mulher) {
+    if (mulher.id === request.params.id) {
+      return mulher;
+    }
+  }
+
+  const mulherEncontrada = mulheres.find(encontraMulher);
+
+  if (request.body.name) {
+    mulherEncontrada.nome = request.body.nome;
+  }
+
+  if (request.body.imagem) {
+    mulherEncontrada.imagem = request.body.imagem;
+  }
+
+  if (request.body.minibio) {
+    mulherEncontrada.minibio = request.body.minibio;
+  }
+
+  response.json(mulheres);
+
+  mulheres.push(novaMulher);
+
+  return response.json(mulheres);
+}
+
+//DELETE
+function deletaMulher(request, response) {
+  function todasMenosEla(mulher) {
+    if (mulher.id !== request.params.id) {
+      return mulher;
+    }
+  }
+
+  const mulheresQueFicam = mulheres.filter(todasMenosEla);
+
+  response.json(mulheresQueFicam);
+}
+
+//PORTA
+function mostraPorta() {
+  console.log("Servidor criado e rodando na porta ", porta);
+}
+
+app.use(router.get("/mulheres", mostraMulheres)); //configurei a rota GET /mulheres
+app.use(router.post("/mulheres", criaMulher)); //configurei a rota POST /mulheres
+app.use(router.patch("/mulheres/:id", corrigeMulher)); //configurei a rota PATCH /mulheres
+app.use(router.delete("/mulheres/:id", deletaMulher)); //configurei a rota DELETE /mulheres
+app.listen(porta, mostraPorta); //servidor ouvindo a porta
